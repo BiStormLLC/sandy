653fa387832c68913a3c8a964caae6688733a953
Jen <jonathan@bistorm.associates>
2017-02-09 11:06:22 -0800

    Fixing directory to clear for streams

--- SLUG/action/stop.php
+++ SLUG/action/stop.php
@@ -3,7 +3,7 @@
 
 $old_path = getcwd();
 chdir('/vagrant/bistorm');
-$output = shell_exec('./ffmpeg-kill /tmp/hlsc /tmp/hlsd');
+$output = shell_exec('./ffmpeg-kill /var/www/hls');
 chdir($old_path);
 echo "<pre>$output</pre>";
 
